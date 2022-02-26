d78a0627fec3b23bb0de506daa1ce5ecb74c1091
Bryan Num <bnum33@gmail.com>
2022-02-26 13:17:03 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Our react App</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
