80de175c7291dae581c4db29baa375654e6a7bfd
Bryan Num <bnum33@gmail.com>
2022-02-28 20:16:44 -0800

    created Navigation component and added Route for Home Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Navigation from "./Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
