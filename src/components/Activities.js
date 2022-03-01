4d487f0e5ab3f1be82955b394f29f30faf9c30b6
Bryan Num <bnum33@gmail.com>
2022-02-28 21:43:19 -0800

    fetched Activities Data

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,8 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { callApi } from "../api";
+
 const Activities = () => {
+  const [activities, setActivities] = useState([]);
+
+  const fetchActivities = async () => {
+    const data = await callApi({ url: "/activities" });
+    setActivities(data);
+  };
+
+  useEffect(() => {
+    fetchActivities();
+  }, []);
+
   return (
-    <div>
+    <>
       <h1>Activities Page</h1>
-    </div>
+      {activities.map((activity) => {
+        return (
+          <div key={activity.id}>
+            {activity.name} {activity.description}
+          </div>
+        );
+      })}
+    </>
   );
 };
 
