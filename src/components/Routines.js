488b19aab4fe337cc14fa5c937cdb51f9ffc53dc
Bryan Num <bnum33@gmail.com>
2022-02-28 21:38:01 -0800

    fetched Routine Data

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { callApi } from "../api";
 
 const Routines = () => {
+  const [routines, setRoutines] = useState([]);
+
+  const fetchRoutines = async () => {
+    const data = await callApi({ url: "/routines" });
+    setRoutines(data);
+  };
+
+  useEffect(() => {
+    fetchRoutines();
+  }, []);
+
   return (
     <div>
       <h1>Routines Page</h1>
+      {routines.map((routine) => {
+        return (
+          <div key={routine.id}>
+            {routine.name} {routine.goal}
+          </div>
+        );
+      })}
     </div>
   );
 };
