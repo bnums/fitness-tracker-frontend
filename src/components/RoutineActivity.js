85f9b126c49b301954dbcf6c4b7120660c391e27
Bryan Num <bnum33@gmail.com>
2022-03-02 10:02:17 -0800

    added activity description to RoutineActivity

--- src/components/RoutineActivity.js
+++ src/components/RoutineActivity.js
@@ -2,6 +2,7 @@ const RoutineActivity = ({ activity }) => {
   return (
     <div className="routine-activity">
       <div id="routine-activity-name">{activity.name}</div>
+      <div id="routine-activity-description">{activity.description}</div>
       <div id="routine-activity-count">count: {activity.count}</div>
       <div id="routine-activity-duration">duration: {activity.duration}</div>
     </div>
